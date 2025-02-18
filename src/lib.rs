c0db0130e76c35799cb4f9ebbf142fcb3bf52ec3
andr515i <andr515i@zbc.dk>
2025-02-18 15:28:13 +0100

    started implementing handle_commands, currently working on the cd command, specifically subfolders.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,79 @@
+use std::collections::HashSet;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-
+pub struct Terminal {
+    cwd: String,
+    directories: HashSet<String>,
 }
 
+#[allow(clippy::new_without_default)]
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("hello, {}", name));
-}
+impl Terminal {
+    /// Constructor for Terminal
+    pub fn new() -> Terminal {
+        let mut directories = HashSet::new();
+        directories.insert("/".to_string());
+        directories.insert("/games".to_string());
+        directories.insert("/projects".to_string());
+        directories.insert("/home".to_string());
+        directories.insert("/test/test".to_string());
 
-#[wasm_bindgen]
-pub fn fib(n: u32) -> u32 {
-    if n == 0 {
-        return 0;
+        Terminal {
+            cwd: "/".to_string(),
+            directories,
+        }
     }
-    if n == 1 {
-        return 1;
-    }
-    fib(n - 1) + fib(n - 2)
-}
-
-#[wasm_bindgen]
-pub fn handle_command(command: &str) -> String {
-    match command {
-        "help" => "help: display this help message\n".to_string(),
-        "about" => "I am a software developer, working primarily in C#. I have recently begun to learn Rust, which is also what I have used to make most of the logic on this site!\n".to_string(),
-        _ => "unknown command".to_string(),
+    /// takes in a string command and returns the output as a string
+    pub fn handle_command(&mut self, command: &str) -> Result<String, JsValue> {
+        match command.trim() {
+            "help" => {
+                Ok(
+                "help: display this help message\n
+                    about: prints information about the author of this website.\n
+                    projects: prints information about my most recent projects.\n
+                    contact: prints all the current ways you can contact me.\n"
+                    .to_string(),
+                )
+            },
+            "about" => {
+                Ok(
+                "I am a software developer, working primarily in C#. I have recently begun to learn Rust, which is also what I have used to make most of the logic on this site!\n"
+                    .to_string(),
+                )
+            },
+            "projects" => {
+                Ok(
+                "I have a few projects on my GitHub (https://www.github.com/andr515i), but most noteworthy are these projects:\n
+                    1. test project at http://www.github.com/andr515i/test\n"
+                    .to_string(),
+                )
+            },
+            "contact" => {
+                Ok(
+                "You can contact me at\n
+                    1. email: andreasmadsen64@gmail.com \n
+                    2. linkedin: https://www.linkedin.com/in/andreas-madsen-35a856152/\n"
+                    .to_string(),
+                )
+            },
+            command if command.starts_with("cd ") => {
+                let new_dir = command.strip_prefix("cd ").unwrap_or("/");
+                if self.directories.contains(new_dir) {
+                    self.cwd = new_dir.to_string();
+                    Ok(format!("changed directory to {}", self.cwd))
+                }
+                    else {
+                        Err(JsValue::from_str("directory not found"))
+                    }
+                }
+            "test" => {
+                Ok("test\n".to_string())
+            }
+            _ => {
+                Ok("unknown command,\n".to_string())
+            }
+        }
     }
 }
--- wasm/src/app/app.component.ts
+++ wasm/src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import __wbg_init, { } from "../assets/pkg/wasm_rust.js"
-
+import __wbg_init, { Terminal } from "../assets/pkg/wasm_rust.js"
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'wasm';
+  terminal_content: string = 'Hello, and welcome to my cv website!';
+  terminal_command: string = '';
+  terminal: any;
   async ngOnInit() {
     await __wbg_init('../assets/pkg/wasm_rust_bg.wasm');
+    this.terminal = Terminal.new();
   }
 
-
+  onEnter() {
+    this.terminal_content += "\n> " + this.terminal_command + '\n';
+    this.terminal_content += this.terminal.handle_command(this.terminal_command);
+    this.terminal_command = '';
+  }
 
 
 }
