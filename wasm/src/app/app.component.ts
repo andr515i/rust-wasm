bdff5c8be839b04fe5c7cec9cd527a2113211de7
andr515i <andr515i@zbc.dk>
2025-02-19 13:30:23 +0100

    made wonderfull progress towards emulating a very, VERY simple file system, which we can naviate around in. i think the next step i want to take is figure out how to do command autocompletion.

--- wasm/src/app/app.component.ts
+++ wasm/src/app/app.component.ts
@@ -12,17 +12,56 @@ export class AppComponent implements OnInit {
   title = 'wasm';
   terminal_content: string = 'Hello, and welcome to my cv website!';
   terminal_command: string = '';
+  terminal_history: string[] = [];
+  history_index: number = 0;
   terminal: any;
   async ngOnInit() {
     await __wbg_init('../assets/pkg/wasm_rust_bg.wasm');
     this.terminal = Terminal.new();
+
   }
 
+  /** when the user sends a command, first we add it to the content of the terimnal, then we send whatever was in the user's input to the terminal to handle it whereafter we clear the input
+  */
   onEnter() {
-    this.terminal_content += "\n> " + this.terminal_command + '\n';
-    this.terminal_content += this.terminal.handle_command(this.terminal_command);
-    this.terminal_command = '';
+    try {
+      this.terminal_content += "\n> " + this.terminal_command + '\n';
+      this.terminal_content += this.terminal.handle_command(this.terminal_command);
+      this.terminal_history.push(this.terminal_command);
+      this.terminal_command = '';
+      this.history_index = 0;
+
+    }
+    catch (error: any) {
+      const error_message = error.toString();
+      console.log(error_message);
+      // if we dont find a directory when using the cd command, we need to make sure we catch that error and handle the cleanup
+      if (error_message === "directory not found") {
+        this.terminal_content += "Error: Directory not found\n";
+        this.terminal_history.push(this.terminal_command)
+        this.terminal_command = '';
+        this.history_index = 0;
+      }
+    }
   }
 
 
+  /** when the user presses the up arrow, we want to go back in the history of commands
+  */
+  onUp() {
+    if (this.terminal_history.length > 0 && this.history_index < this.terminal_history.length) {
+      this.terminal_command = this.terminal_history[this.terminal_history.length - 1 - this.history_index];
+
+      this.history_index++;
+    }
+  }
+  onDown() {
+    if (this.terminal_history.length > 0 && this.history_index > 0) {
+      this.history_index--;
+      this.terminal_command = this.terminal_history[this.history_index];
+    }
+    else {
+      this.terminal_command = '';
+    }
+  }
 }
